218044e118d74e4cd49ca3c5ff9dba10e53eb545
Jacob Ewing <jacob.ewing@gmail.com>
2026-09-12 21:47:41 -0400

    Fixing canvasSprite bugs
    Scrubbed through with an AI assistant looking for bugs.  It's currently looking
    better and in a working state.  It can still use improvements, but we're at a
    good point to commit.

--- canvasSprite.js
+++ canvasSprite.js
@@ -1,9 +1,17 @@
+'use strict';
+
 // let's define trim as we need it here
-if(typeof trim == "undefined"){
-	trim = function(stringToTrim){ return (' ' + stringToTrim).replace(/^\s+|\s+$/g,""); };
+if (typeof trim === "undefined") {
+    var trim = function(stringToTrim) {
+        return String(stringToTrim).replace(/^\s+|\s+$/g, "");
+    };
 }
 
 var cSprite = function(newTemplate){
+    	if(!(this instanceof cSprite)) {
+		return new cSprite(newTemplate);
+	}
+
 	for(var n in cSprite.defaults){
 		this[n] = cSprite.defaults[n];
 	}
@@ -37,6 +45,12 @@ cSprite.defaults = {
 	imageWidth : 0,
 	imageHeight : 0,
 
+	currentFrameTime : 0,
+	sequenceIterations : 0,
+	sequenceCallback : null,
+	sequenceMethod : 'auto',
+	sequenceFrameRate : 0,
+
 
 	// parent/child sprite management variables
 	numChildren : 0,
@@ -47,32 +61,36 @@ cSprite.prototype.setTemplate = function(template){
 	this.template = template;
 	this.image = template.image;
 
-	this.imageWidth = this.image.width;
-	this.imageHeight = this.image.height;
+	this.imageWidth = this.image.naturalWidth || this.image.width;
+	this.imageHeight = this.image.naturalHeight || this.image.height;
 
 	this.frameWidth = template.frameWidth;
 	this.frameHeight = template.frameHeight;
 };
 
-cSprite.prototype.setFrame = function(frameName){
-	try{
-		this.frameName = frameName;
-		this.frame = this.template.frames[frameName];
-	}catch(e){
-		console.log(e);
-		debugger;
-		
+cSprite.prototype.setFrame = function(frameName) {
+	this.frameName = frameName;
+	this.frame = this.template.frames[frameName];
+	if (!this.frame) {
+		throw new Error("Frame not found: " + frameName);
 	}
 };
 
-cSprite.prototype.startSequence = function(sequenceName){
-	if(this.template.sequences[sequenceName] != undefined){
-		this.animating = true;
-		this.currentSequenceName = sequenceName;
-		this.currentSequence = this.template.sequences[sequenceName];
-		this.frameIndex = 0; // <-- fixme, allow this to be passed as an argument to this function
-		this.setFrame(this.currentSequence.frames[this.frameIndex]);
-	}
+cSprite.prototype.startSequence = function(sequenceName, callback) {
+    var seq = this.template.sequences[sequenceName];
+    if (!seq) return;
+
+    this.animating = true;
+    this.currentSequenceName = sequenceName;
+    this.currentSequence = seq;          // reference to the immutable definition
+    this.frameIndex = 0;
+    this.currentFrameTime = 0;
+    this.sequenceIterations = (seq.iterations !== undefined) ? seq.iterations : 0;
+    this.sequenceCallback = callback || seq.callback || null;
+    this.sequenceMethod = seq.method || 'auto';
+    this.sequenceFrameRate = seq.frameRate || this.template.defaultFrameRate;
+
+    this.doSequenceStep();
 };
 
 cSprite.prototype.rotate = function(angle){
@@ -117,71 +135,78 @@ cSprite.prototype.drawArea = function(context, drawx, drawy, readx1, ready1, wid
 	context.restore();
 }
 
-cSprite.prototype.draw = function(context, params){
-	if(params == undefined) params = {};
+cSprite.prototype.draw = function(context, params) {
+	if (!params) params = {};
+
 	var x = this.position.x + this.drawOffset.x;
 	var y = this.position.y + this.drawOffset.y;
-	if(this.frame != undefined){
-		var frameX = this.frame.x;
-		var frameY = this.frame.y;
-		var frameWidth = this.frame.width;
-		var frameHeight = this.frame.height;
-	}
-	var n;
 	var drawScale = this.scale;
-	for(n in params){
-		switch(n){
+
+	var frameX = 0;
+	var frameY = 0;
+	var frameWidth = this.imageWidth;
+	var frameHeight = this.imageHeight;
+	var frameCenterX = this.template.centerx;
+	var frameCenterY = this.template.centery;
+
+	if (this.frame) {
+		frameX = this.frame.x;
+		frameY = this.frame.y;
+		frameWidth = this.frame.width;
+		frameHeight = this.frame.height;
+		frameCenterX = this.frame.centerx;
+		frameCenterY = this.frame.centery;
+	}
+
+	for (var n in params) {
+		if (!params.hasOwnProperty(n)) continue;
+		switch (n) {
 			case 'x': x = params[n] + this.drawOffset.x; break;
 			case 'y': y = params[n] + this.drawOffset.y; break;
-			case 'scale' : drawScale = params[n]; break;
-			case 'frameWidth' : frameWidth = params[n]; break;
-			case 'frameHeight' : frameHeight = params[n]; break;
-			case 'frameX' : frameX = params[n]; break;
-			case 'frameY' : frameY = params[n]; break;
+			case 'scale': drawScale = params[n]; break;
+			case 'frameWidth': frameWidth = params[n]; break;
+			case 'frameHeight': frameHeight = params[n]; break;
+			case 'frameX': frameX = params[n]; break;
+			case 'frameY': frameY = params[n]; break;
 		}
 	}
+
 	context.save();
-	if(this.frame.drawOffset != undefined){
-		x += this.frame.drawOffset.x;
-		y += this.frame.drawOffset.y;
-	}
-	context.translate(x * drawScale, y * drawScale);
-	context.rotate(this.rotation);
-
-	context.scale(drawScale, drawScale);
-	if(this.frame == null) {
-		context.translate(-this.template.centerx, -this.template.centery);
-	}else{
-		context.translate(-this.frame.centerx, -this.frame.centery);
-	}
-
-	for(n = 0; n < this.numChildren; n++){
-		if(this.children[n].zIndex > this.zIndex){
-			this.children[n].draw(context);
+	try {
+		if (this.frame && this.frame.drawOffset) {
+			x += this.frame.drawOffset.x;
+			y += this.frame.drawOffset.y;
 		}
-	}
-	context.drawImage(
-		this.image,
-		frameX, frameY,
-		frameWidth, frameHeight,
-		0, 0,
-		frameWidth, frameHeight
-	);
 
-	for(n = 0; n < this.numChildren; n++){
-		if(this.children[n].zIndex <= this.zIndex){
-			this.children[n].draw(context);
+		context.translate(x * drawScale, y * drawScale);
+		context.rotate(this.rotation);
+		context.scale(drawScale, drawScale);
+		context.translate(-frameCenterX, -frameCenterY);
+
+		for (var i = 0; i < this.children.length; i++) {
+			if (this.children[i].zIndex > this.zIndex) {
+				this.children[i].draw(context);
+			}
 		}
-	}
 
-	if(this.animating){
-		this.frameIndex = (this.frameIndex + 1) % this.currentSequence.frames.length;
-		this.frameName = this.currentSequence.frames[this.frameIndex];
-		this.setFrame(this.frameName);
+		context.drawImage(
+				this.image,
+				frameX, frameY,
+				frameWidth, frameHeight,
+				0, 0,
+				frameWidth, frameHeight
+				);
+
+		for (var j = 0; j < this.children.length; j++) {
+			if (this.children[j].zIndex <= this.zIndex) {
+				this.children[j].draw(context);
+			}
+		}
+
+	} finally {
+		context.restore();
 	}
-	context.restore();
 };
-
 cSprite.prototype.setScale = function(newScale){
 	this.scale = newScale;
 	/*
@@ -211,15 +236,8 @@ cSprite.prototype.setScale = function(newScale){
 
 cSprite.prototype.setPosition = function(x, y, useScale){
 	this.centerx = this.centery = 0;
-	var offset = {'x' : 0, 'y' : 0};
 	this.position.x = x;
 	this.position.y = y;
-	/*
-	if(useScale){
-		this.position.x *= this.scale;
-		this.position.y *= this.scale;
-	}
-	*/
 	if(this.frame != undefined){
 		this.centerx = this.frame.centerx;
 		this.centery = this.frame.centery;
@@ -227,33 +245,32 @@ cSprite.prototype.setPosition = function(x, y, useScale){
 };
 
 // parent/child sprite management functions
-cSprite.prototype.detach = function(oldChild){
-	if(oldChild == undefined){
-		// separate this sprite from it's parent
-		if(this.myParent) this.myParent.detach(this);
-	}else{
-		// separate the specified child from it's parent
-		var foundit = false;
-		for(var n in this.children){
-			if(this.children[n] == oldChild){
-				foundit = true;
-			}else if(foundit){
-				this.children[n - 1] = this.children[n];
-			}
+
+cSprite.prototype.detach = function(oldChild) {
+	if (oldChild === undefined) {
+		if (this.myParent) {
+			this.myParent.detach(this);
 		}
-		if(foundit){
-			this.numChildren--;
-			this.children[this.numChildren].myParent = 0;
-			this.children[this.numChildren] = null;
-		}
+		this.myParent = 0;
+		return;
+	}
+
+	var index = this.children.indexOf(oldChild);
+	if (index !== -1) {
+		this.children.splice(index, 1);
+		this.numChildren = this.children.length;
+		oldChild.myParent = 0;
 	}
 };
 
-cSprite.prototype.attach = function(newChild){
+cSprite.prototype.attach = function(newChild) {
+	if (newChild === this) {
+		throw new Error("Cannot attach a sprite to itself");
+	}
 	newChild.detach();
 	newChild.myParent = this;
-	this.children[this.numChildren] = newChild;
-	this.numChildren++;
+	this.children.push(newChild);
+	this.numChildren = this.children.length;
 };
 
 cSprite.prototype.attachTo = function(newParent){
@@ -261,59 +278,58 @@ cSprite.prototype.attachTo = function(newParent){
 	newParent.attach(this);
 };
 
-cSprite.prototype.doSequenceStep = function(params){
-	if(params == undefined){
-		if(this.currentSequence != undefined){
-			params = this.currentSequence;
-		}else{
-			return;
-		}
-	}
+cSprite.prototype.doSequenceStep = function() {
+	if (!this.animating || !this.currentSequence) return;
 
-	if(!params || params.stop == true){
+	var seq = this.currentSequence;
+	var frames = seq.frames;
+	if (!frames || frames.length === 0) {
+		this.animating = false;
 		return;
 	}
 
-	var doNextFrame = (params.method == 'auto');
-	var animDelay = params.frameRate;
+	// Set the current frame
+	this.setFrame(frames[this.frameIndex]);
 
-	if(params.frameTimes != undefined){
-		animDelay = params.frameTimes[params.currentFrameTime];
-		params.currentFrameTime = (params.currentFrameTime + 1) % params.frameTimes.length;
-	}
-
-	if(params.currentFrame == undefined){
-		params.currentFrame = 0;
-	}
-
-	this.setFrame(params.frames[params.currentFrame]);
-	params.currentFrame++;
-	
-	if(params.currentFrame == params.frames.length){
-		if(params.iterations == 1){
-			doNextFrame = false;
+	// Advance the frame index
+	this.frameIndex++;
+	if (this.frameIndex >= frames.length) {
+		// End of sequence reached
+		if (this.sequenceIterations === 1) {
+			// Play once and stop
+			this.animating = false;
 			this.currentSequence = null;
-			params.callback();
-		}else if(params.iterations == 0){
-			params.currentFrame = 0;
-			this.currentSequence = params;
-		}else{
-			params.currentFrame = 0;
-			params.iterations--;
-			this.currentSequence = params;
+			if (typeof this.sequenceCallback === 'function') {
+				this.sequenceCallback.call(this);
+			}
+			return;
+		} else if (this.sequenceIterations === 0) {
+			// Infinite loop
+			this.frameIndex = 0;
+		} else {
+			// Finite iterations remaining
+			this.sequenceIterations--;
+			this.frameIndex = 0;
 		}
 	}
-	if(doNextFrame){
-		var me = this;
-		setTimeout(function(){me.doSequenceStep(params)}, animDelay);
-	}
-	return params;
-};
 
+	// Schedule the next frame if the method is 'auto'
+	if (this.sequenceMethod === 'auto') {
+		var delay = this.sequenceFrameRate;
+		if (seq.frameTimes && seq.frameTimes.length > 0) {
+			delay = seq.frameTimes[this.currentFrameTime];
+			this.currentFrameTime = (this.currentFrameTime + 1) % seq.frameTimes.length;
+		}
+		setTimeout(() => this.doSequenceStep(), delay);
+	}
+};
 
 ////////////////////////////////////////////////////////////////////////////////////
 
 var spriteSet = function(filename, callback){
+	if (!(this instanceof spriteSet)) {
+		return new spriteSet(filename, callback);
+	}
 
 	for(var n in spriteSet.defaults){
 		this[n] = spriteSet.defaults[n];
@@ -337,7 +353,9 @@ spriteSet.defaults = {
 };
 
 spriteSet.prototype.newSprite = function(){
-	return new cSprite(this);
+	var sprite = new cSprite(this);
+	sprite.scale = this.scale;
+	return sprite;
 };
 
 spriteSet.prototype.setScale = function(scale){
@@ -358,7 +376,7 @@ spriteSet.prototype.load = function(fileName, callback){
 			if (this.readyState == 4 && this.status == 200) {
 
 				try{
-					data = JSON.parse(this.responseText);
+					var data = JSON.parse(this.responseText);
 					me.loadJSON(data, callback);
 
 				}catch(e){
@@ -383,8 +401,11 @@ spriteSet.prototype.addFrame = function(id, params){
 	};
 	for(n in params){
 		switch(trim(n).toLowerCase()){
-			case 'width': case 'height':
-				newFrame[n] = 1 * params[n];
+			case 'width':
+				newFrame.width = 1 * params[n];
+				break;
+			case 'height':
+				newFrame.height = 1 * params[n];
 				break;
 			case 'x': case 'left':
 				newFrame['x'] = 1 * newFrame['x'] + 1 * params[n];
@@ -420,18 +441,21 @@ spriteSet.prototype.loadJSON = function(data, callback){
 	var rfunc;
 
 	if(key != undefined){
-		val = data[key];
+		var val = data[key];
 		delete data[key];
-		rfunc = function(){setTimeout(me.loadJSON(data, callback), 0);};
-		switch(key){
+		rfunc = () => {
+			setTimeout( () => { me.loadJSON(data, callback);}, 0);
+		};
+
+		switch(key.toLowerCase()){
 			case 'image':
 				this.setImage(val, rfunc);
 				break;
-			case 'frameWidth': case 'framewidth':
+			case 'framewidth':
 				this.frameWidth = 1 * val;
 				rfunc();
 				break;
-			case 'frameHeight': case 'frameheight':
+			case 'frameheight':
 				this.frameHeight = 1 * val;
 				rfunc();
 				break;
@@ -479,13 +503,13 @@ spriteSet.prototype.load_sequences = function(data){
 			'frameRate': this.defaultFrameRate
 		};
 		for(param in data[name]){
-			switch(param){
+			switch(param.toLowerCase()){
 				case 'frames':
 					for(n = 0; n < data[name][param].length; n++){
 						newSequence.frames[n] = data[name][param][n];
 					}
 					break;
-				case 'framerate': case 'frameRate':
+				case 'framerate':
 					newSequence.frameRate = 1 * data[name][param];
 					break;
 			}
@@ -510,8 +534,11 @@ spriteSet.prototype.load_frames = function(data){
 		this.frameNames[this.frameNames.length] = name;
 		for(arg in data[name]){
 			switch(arg.toLowerCase()){
-				case 'width': case 'height':
-					this.frames[name][arg] = 1 * data[name][arg];
+				case 'width':
+					this.frames[name].width = 1 * data[name][arg];
+					break;
+				case 'height':
+					this.frames[name].height = 1 * data[name][arg];
 					break;
 				case 'x':
 					this.frames[name]['x'] += this.frameWidth * data[name][arg];
@@ -532,7 +559,7 @@ spriteSet.prototype.load_frames = function(data){
 					this.frames[name]['centery'] = 1 * data[name][arg];
 					break;
 				case 'drawoffset':
-					this.frames[name]['drawOffset'] = data[name][arg];
+					this.frames[name].drawOffset = data[name][arg];
 					break;
 			}
 		}
